1a5ef55a002accfe39320bcd5ed42ab1e2f553c4
Xuan Thang <thangthitotnghiep@gmail.com>
2026-04-08 23:47:48 +0700

    fist commmit

---
+++ server/server/src/main/java/org/group17/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.group17;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
